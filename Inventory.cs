1fd08fd96f666f0543a21d9eea512c9c2bd58b9d
aruensia <aruensia@naver.com>
2025-01-01 22:48:17 +0900

    Initial commit

---
+++ Inventory.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inventory
+{
+    class Weapon
+    {
+        public bool[,] weaponBool;
+
+        public Weapon()
+        {
+
+        }
+
+        public Weapon(bool[,] weaponBool)
+        {
+            this.weaponBool = weaponBool;
+        }
+    }
+
+    internal class Inventory
+    {
+        public bool[,] bools;
+        public int setXNum = 0;
+        public int setYNum = 0;
+
+        public void SetInventory(int X, int Y)
+        {
+            bools = new bool[X, Y];
+        }
+
+        public bool[,] SetInvenArr(Inventory inventory)
+        {
+            bool[,] arr;
+
+            setXNum = 5;
+            setYNum = 5;
+
+            inventory.SetInventory(setXNum, setYNum);
+            return arr = inventory.bools;
+        }
+
+        public bool[,] SetItem(bool[,] myinventory, Weapon weapon)
+        {
+            int itemXSize = int.Parse(Console.ReadLine());
+            int itemYSize = int.Parse(Console.ReadLine());
+
+            bool[,] arr = new bool[itemXSize, itemYSize];
+            
+            weapon.weaponBool = arr;
+            return weapon.weaponBool;
+        }
+
+        public bool[,] SellctAddItemPos()
+        {
+            Console.WriteLine("배치할 위치");
+            int bools1 = int.Parse(Console.ReadLine());
+            int bools2 = int.Parse(Console.ReadLine());
+            bool[,] SellctBoolPos = new bool[bools1, bools2];
+
+            return SellctBoolPos;
+        }
+
+        public void AddItem(bool[,] bools, bool[,] weaponbools, bool[,] SellectBoolPos)
+        {
+            if (weaponbools != null)
+            {
+                int sumnum1 = SellectBoolPos.GetLength(0) + weaponbools.GetLength(0);
+                int sumnum2 = SellectBoolPos.GetLength(1) + weaponbools.GetLength(1);
+
+
+                Console.WriteLine(sumnum1);
+                Console.WriteLine(sumnum2);
+
+
+                if (SellectBoolPos.GetLength(0)+weaponbools.GetLength(0) < bools.GetLength(0) || SellectBoolPos.GetLength(1) + weaponbools.GetLength(1) < bools.GetLength(1))
+                {
+                    for (int i = 0; i < weaponbools.GetLength(0); i++)
+                    {
+                        for (int j = 0; j < weaponbools.GetLength(1); j++)
+                        {
+                            bools[i, j] = true;
+                        }
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("아이템이 인벤토리에 안들어갑니다.");
+                }
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inventory
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool[,] setbools;
+            bool[,] invenArr;
+            bool[,] CheckPos;
+
+            Weapon weapon = new Weapon();
+            Inventory inventory = new Inventory();
+
+            invenArr = inventory.SetInvenArr(inventory);
+            setbools = inventory.SetItem(invenArr, weapon);
+
+            Console.WriteLine("------5*5 이하 크기의 아이템을 넣음------");
+            CheckPos = inventory.SellctAddItemPos();
+            inventory.AddItem(invenArr, setbools, CheckPos);
+
+            Console.WriteLine($"{invenArr[0, 0]},{invenArr[0, 1]},{invenArr[0, 2]},{invenArr[0, 3]},{invenArr[0, 4]}");
+            Console.WriteLine($"{invenArr[1, 0]},{invenArr[1, 1]},{invenArr[1, 2]},{invenArr[1, 3]},{invenArr[1, 4]}");
+            Console.WriteLine($"{invenArr[2, 0]},{invenArr[2, 1]},{invenArr[2, 2]},{invenArr[2, 3]},{invenArr[2, 4]}");
+            Console.WriteLine($"{invenArr[3, 0]},{invenArr[3, 1]},{invenArr[3, 2]},{invenArr[3, 3]},{invenArr[3, 4]}");
+            Console.WriteLine($"{invenArr[4, 0]},{invenArr[4, 1]},{invenArr[4, 2]},{invenArr[4, 3]},{invenArr[4, 4]}");
+        }
+    }
+}
